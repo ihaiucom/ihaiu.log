161cffd6242c0c6d16411865f829dad5436453c9
zengfeng75 <zengfeng75@qq.com>
2017-06-12 03:53:01 +0800

    no message

---
+++ Log/Assets/ihaiu.Log/Scirpts/MDebugCacheItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MDebugCacheItem
+{
+	public LogType 		logType;
+	public string		datetime;
+	public int 			flagId;
+	public string		tag;
+	public string		msg;
+
+}
